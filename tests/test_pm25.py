99c31610fa77d94d04ce90c14ce8fc9d0560fb55
Andrew Wilkinson <andrewjwilkinson@gmail.com>
2025-01-12 16:56:24 +0000

    Update test_pm25.py

--- tests/test_pm25.py
+++ tests/test_pm25.py
@@ -23,8 +23,11 @@ from breakoutgardenexporter import PM25Sensor, Metrics
 
 import breakoutgardenexporter.pm25
 
+# Needed to fix tests in GitHub actions.
 if not hasattr(breakoutgardenexporter.pm25.board, "SCL"):
     breakoutgardenexporter.pm25.board.SCL = 0
+if not hasattr(breakoutgardenexporter.pm25.board, "SCA"):
+    breakoutgardenexporter.pm25.board.SCA = 0
 
 SAMPLE_DATA = {'pm10 standard': 2,
                'pm25 standard': 3,
