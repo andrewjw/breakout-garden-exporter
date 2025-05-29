218883dfaf019af059676f5c7a15946b471adbe3
Andrew Wilkinson <andrewjwilkinson@gmail.com>
2025-05-29 14:04:13 +0000

    feat: Remove support for Python 3.8.
    BREAKING CHANGE: Removed support for Python 3.8.

--- setup.py
+++ setup.py
@@ -36,7 +36,6 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     scripts=["bin/breakout-garden-exporter"],
     classifiers=[
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
@@ -44,6 +43,6 @@ setuptools.setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8',
+    python_requires='>=3.9',
     install_requires=requirements,
 )
